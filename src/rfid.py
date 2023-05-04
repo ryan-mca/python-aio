8bd07939ba79101864197cb13de1ae3725fdf866
Ryan McAlpine <mcalpine06@outlook.com>
2023-05-04 23:20:49 +1000

    once again trying to fix bugs

--- src/rfid.py
+++ src/rfid.py
@@ -3,14 +3,17 @@ from PiicoDev_Unified import sleep_ms
 
 def read(detailed=False):
     print("(i) Setting up RFID")
-    rfid = PiicoDev_RFID()
+    try:
+        rfid = PiicoDev_RFID()
 
-    print("(i) Place RFID device near RFID reader")
+        print("(i) Place RFID device near RFID reader")
 
-    while True:
-        if rfid.tagPresent():
-            id = rfid.readID(detail=detailed)
+        while True:
+            if rfid.tagPresent():
+                id = rfid.readID(detail=detailed)
 
-            print(id)
-    
-        sleep_ms(100)
+                print(id)
+        
+            sleep_ms(100)
+    except OSError:
+        pass
