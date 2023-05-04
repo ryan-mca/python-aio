b73548fbd8aedd9d0b077acc69c4c2d88eff3fc4
Ryan McAlpine <mcalpine06@outlook.com>
2023-05-04 23:26:55 +1000

    Figured out bug, testing

--- src/rfid.py
+++ src/rfid.py
@@ -3,17 +3,20 @@ from PiicoDev_Unified import sleep_ms
 
 def read(detailed=False):
     print("(i) Setting up RFID")
-    try:
-        rfid = PiicoDev_RFID()
+    while True:
+        try:
+            rfid = PiicoDev_RFID()
 
-        print("(i) Place RFID device near RFID reader")
+            print("(i) Place RFID device near RFID reader")
 
-        while True:
-            if rfid.tagPresent():
-                id = rfid.readID(detail=detailed)
+            while True:
+                if rfid.tagPresent():
+                    id = rfid.readID(detail=detailed)
 
-                print(id)
+                    print(id)
+            
+                sleep_ms(100)
+        except OSError:
+            print("(-) Make sure your RFID device is connected properly")
+            sleep_ms(1000)
         
-            sleep_ms(100)
-    except OSError:
-        pass
